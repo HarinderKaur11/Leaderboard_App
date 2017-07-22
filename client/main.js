37cd5e81dcc3e6d98c6d7a7d23b80d0c88b32a28
Harinder Kaur <snazyahuja11@gmail.com>
2017-07-21 23:56:30 -0400

    Implement each block

--- client/main.js
+++ client/main.js
@@ -3,10 +3,7 @@ PlayersList=new Mongo.Collection("players");
 if(Meteor.isClient){
 	Template.leaderboard.helpers({
 		'player': function(){
-			return 'player helper function';
-		},
-		'secondhelper': function(){
-			return 'second helper function';
+			return PlayersList.find();
 		}
 	});
 }
